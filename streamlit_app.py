2e0eb794510cd7919d186d14750fc98a9874871b
sashaa-11 <vivsashaa@gmail.com>
2026-01-08 18:37:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredients_list:
       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
       st.subheader(fruit_chosen + 'Nutrition Information')
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-      sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+      smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
   
     my_insert_stmt = (
         "insert into smoothies.public.orders (ingredients, name_on_order) "
